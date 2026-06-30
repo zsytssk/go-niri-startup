6c9d2274d14c437d3cfafb698fe7f76b60b131a8
zsytssk <zsytssk@gmail.com>
2026-06-30 09:37:34 +0800

    [save] [master]

--- command/action/cmdAction/relaunch.go
+++ command/action/cmdAction/relaunch.go
@@ -44,10 +44,11 @@ func CmdAction(cmd string) error {
 		return nil
 	}
 
+	cmd = strings.Replace(cmd, "Relaunch", "", 1)
 	cmd = strings.ToLower(cmd)
 	cmd = fmt.Sprintf("(pkill %s 2>/dev/null || true) && %s &", cmd, cmd)
-	fmt.Println(cmd)
 	err = runNiriSh(cmd)
+	fmt.Println(cmd, err)
 	if err != nil {
 		return err
 	}
